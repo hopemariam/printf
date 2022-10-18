d88147af44a2f940766eb82649f86b81fa291e65
hopemariam <hope2006ng.ts@gmail.com>
2022-10-18 13:52:12 -0700

    commit

--- get_precision.c
+++ get_precision.c
@@ -39,17 +39,3 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
-
-/**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
-int is_digit(char c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-
-	return (0);
-}
